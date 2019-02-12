c9ab7d72865dfbcaf8a983c90c45a80a36ba4320
devshankha <45039485+devshankha@users.noreply.github.com>
2019-02-12 19:04:26 +0530

    Add files via upload

---
+++ Graph/Graph.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+
+public class Graph {
+	int noOfVertices;
+	LinkedList<Integer> list[];
+
+	Graph(int v) {
+		noOfVertices = v;
+		list = new LinkedList[v];
+		for (int i = 0; i < v; i++) {
+			list[i] = new LinkedList<Integer>();
+		}
+
+	}
+	void addEdge(int src,int dst) {
+		list[src].add(dst);
+		list[dst].add(src);
+		
+	}
+	void printGraph( ) {
+		
+		for (int i=0; i < list.length;i++) {
+			System.out.println("Adjacency list of vertex " + i);
+			System.out.print("head");
+			for (Integer m:list[i]) {
+				System.out.print(" -> "+m); 
+				
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Graph grh = new Graph(4);
+		grh.addEdge(0, 1);
+		grh.addEdge(1,2);
+		grh.addEdge(2,3);
+		grh.addEdge(3,1);
+		grh.printGraph();
+		
+	}
+
+}
