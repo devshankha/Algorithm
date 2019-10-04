222220e7a6f97735c3561fa75d9a36fe901bc14e
devshankha <45039485+devshankha@users.noreply.github.com>
2019-10-04 21:03:27 +0530

    Add files via upload

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,86 +1,87 @@
-package mine;
-//Program to check if one is a subtree another
+package ww.ww;
+class Node { 
+    char data; 
+    Node left, right; 
+  
+    Node(char item) 
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+} 
 
-class Node {
-	int data;
-	Node left;
-	Node right;
-
-	Node(int d) {
-		data = d;
+class BinaryTree { 
+	Node root; 
+    static int preIndex = 0; 
+  
+    /* Recursive function to construct binary of size len from 
+       Inorder traversal in[] and Preorder traversal pre[]. 
+       Initial values of inStrt and inEnd should be 0 and len -1.   
+       The function doesn't do any error checking for cases where  
+       inorder and preorder do not form a tree */
+    Node buildTree(char in[], char pre[], int inStrt, int inEnd) 
+    { 
+        if (inStrt > inEnd) 
+            return null; 
+  
+        /* Pick current node from Preorder traversal using preIndex 
+           and increment preIndex */
+        Node tNode = new Node(pre[preIndex++]); 
+  
+        /* If this node has no children then return */
+        if (inStrt == inEnd) 
+            return tNode; 
+  
+        /* Else find the index of this node in Inorder traversal */
+        int inIndex = search(in, inStrt, inEnd, tNode.data); 
+  
+        /* Using index in Inorder traversal, construct left and 
+           right subtress */
+        tNode.left = buildTree(in, pre, inStrt, inIndex - 1); 
+        tNode.right = buildTree(in, pre, inIndex + 1, inEnd); 
+  
+        return tNode; 
+    } 
+    /* UTILITY FUNCTIONS */
+    
+    /* Function to find index of value in arr[start...end] 
+     The function assumes that value is present in in[] */
+    int search(char arr[], int strt, int end, char value) 
+    { 
+        int i; 
+        for (i = strt; i <= end; i++) { 
+            if (arr[i] == value) 
+                return i; 
+        } 
+        return i; 
+    } 
+  
+    /* This funtcion is here just to test buildTree() */
+    void printInorder(Node node) 
+    { 
+        if (node == null) 
+            return; 
+  
+        /* first recur on left child */
+        printInorder(node.left); 
+  
+        /* then print the data of node */
+        System.out.print(node.data + " "); 
+  
+        /* now recur on right child */
+        printInorder(node.right); 
+    } 
+    public static void main(String[] args) {
+    	BinaryTree tree = new BinaryTree(); 
+        char in[] = new char[] { 'D', 'B', 'E', 'A', 'F', 'C' }; 
+        char pre[] = new char[] { 'A', 'B', 'D', 'E', 'C', 'F' }; 
+        int len = in.length; 
+        Node root = tree.buildTree(in, pre, 0, len - 1); 
+  
+        // building the tree by printing inorder traversal 
+        System.out.println("Inorder traversal of constructed tree is : "); 
+        tree.printInorder(root); 
 	}
-}
-
-class BinaryTree {
-	Node root1, root2;
-
-	boolean areIdentical(Node root1, Node root2) {
-
-		/* base cases */
-		// These two are very cool checks, to see if
-		//either BOTH are null or NONE of them are null 
-		if (root1 == null && root2 == null)
-			return true;
-
-		if (root1 == null || root2 == null)
-			return false;
-
-		/*
-		 * Check if the data of both roots is same and data of left and right subtrees
-		 * are also same
-		 */
-		return (root1.data == root2.data && areIdentical(root1.left, root2.left)
-				&& areIdentical(root1.right, root2.right));
-	}
-
-	boolean isSubtree(Node T, Node S) {
-		/* base cases */
-		if (S == null)
-			return true;
-
-		if (T == null)
-			return false;
-
-		/* Check the tree with root as current node */
-		if (areIdentical(T, S))
-			return true;
-
-		/*
-		 * If the tree with root as current node doesn't match then try left and right
-		 * subtrees one by one
-		 */
-		return isSubtree(T.left, S) || isSubtree(T.right, S);
-	}
-
-	public static void main(String[] args) {
-		BinaryTree tree = new BinaryTree();
-
-		// TREE 1
-		/*
-		 * Construct the following tree 26 / \ 10 3 / \ \ 4 6 3 \ 30
-		 */
-
-		tree.root1 = new Node(26);
-		tree.root1.right = new Node(3);
-		tree.root1.right.right = new Node(3);
-		tree.root1.left = new Node(10);
-		tree.root1.left.left = new Node(4);
-		tree.root1.left.left.right = new Node(30);
-		tree.root1.left.right = new Node(6);
-
-		// TREE 2
-		/*
-		 * Construct the following tree 10 / \ 4 6 \ 30
-		 */
-
-		tree.root2 = new Node(10);
-		tree.root2.right = new Node(6);
-		tree.root2.left = new Node(4);
-		tree.root2.left.right = new Node(30);
-
-		if (tree.isSubtree(tree.root1, tree.root2))
-			System.out.println("Tree 2 is subtree of Tree 1 ");
-		else
-			System.out.println("Tree 2 is not a subtree of Tree 1");
-	}
+  
+    
 }
