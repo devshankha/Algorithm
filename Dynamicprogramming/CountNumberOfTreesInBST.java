e16d0006222187c52a17c0485fa5120dbbf51713
devshankha <45039485+devshankha@users.noreply.github.com>
2020-01-15 17:05:32 +0530

    Add files via upload

---
+++ Dynamicprogramming/CountNumberOfTreesInBST.java
@@ -0,0 +1,19 @@
+//count the number of BST that can be formed with
+//n distinct keys
+public class CountNumberOfTreesInBST {
+	public static int countTrees(int n){
+        int T[] = new int[n+1];
+        T[0] = 1;
+        T[1] = 1;
+        for(int i=2; i <= n; i++){
+            for(int j=0; j <i; j++){
+                T[i] += T[j]*T[i-j-1];
+            }
+        }
+        return T[n];
+    }
+	public static void main(String[] args) {
+		System.out.println(CountNumberOfTreesInBST.countTrees(4));
+	}
+
+}
