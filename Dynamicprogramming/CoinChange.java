39db6a0e79641516b1fcc886a012b6298f044b67
devshankha <45039485+devshankha@users.noreply.github.com>
2020-02-01 11:39:17 +0530

    Update CoinChange.java

--- Dynamicprogramming/CoinChange.java
+++ Dynamicprogramming/CoinChange.java
@@ -37,6 +37,40 @@ public class CoinChange {
 		return table[m][n];  
 		
 	}
+	//same technique but uses a 1 d array instead of going for a 2d array
+	static long countWays(int S[], int m, int n) {
+		// Time complexity of this function: O(mn)
+		// Space Complexity of this function: O(n)
+
+		// table[i] will be storing the number of solutions
+		// for value i. We need n+1 rows as the table is
+		// constructed in bottom up manner using the base
+		// case (n = 0)
+		long[] table = new long[n + 1];
+
+		// Initialize all table values as 0
+		Arrays.fill(table, 0); // O(n)
+		//System.out.println(Arrays.toString(table));
+		// Base case (If given value is 0)
+		table[0] = 1;
+
+		// Pick all coins one by one and update the table[]
+		// values after the index greater than or equal to
+		// the value of the picked coin
+		for (int i = 0; i < m; i++) {
+			for (int j = S[i]; j <= n; j++) {
+				//System.out.print(j + " ");
+				// Here table[j] denotes the number of ways sum of J can be 
+				//created using coin s[i], this will be number of ways sum of J-sum[i] 
+				// can be created using coin s[i]
+				//be created using 
+				table[j] += table[j - S[i]];
+			}
+			System.out.println(Arrays.toString(table));
+		}
+		 System.out.println("The final array "+Arrays.toString(table));
+		return table[n];
+	}
 	
 	static int recCount(int S[], int m, int n) {
 		// If n is 0 then there is 1 solution  
