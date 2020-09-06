c30ee364f13a78b982045d9889eb422209910d72
devshankha <45039485+devshankha@users.noreply.github.com>
2020-09-06 16:24:39 +0530

    Update Graph.java

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -17,6 +17,43 @@ public class Graph {
 		list[dst].add(src);
 		
 	}
+	
+		void BFS(int s) 
+    { 
+		 // Mark all the vertices as not visited(By default 
+        // set as false) 
+        boolean visited[] = new boolean[noOfVertices]; 
+  
+        // Create a queue for BFS 
+        LinkedList<Integer> queue = new LinkedList<Integer>(); 
+  
+        // Mark the current node as visited and enqueue it 
+        visited[s]=true; 
+        queue.add(s); 
+        while (queue.size() != 0) 
+        { 
+            // Dequeue a vertex from queue and print it 
+            s = queue.poll(); 
+            System.out.print(s+" "); 
+  
+            // Get all adjacent vertices of the dequeued vertex s 
+            // If a adjacent has not been visited, then mark it 
+            // visited and enqueue it 
+            Iterator<Integer> i = list[s].listIterator(); 
+            while (i.hasNext()) 
+            { 
+                int n = i.next(); 
+                if (!visited[n]) 
+                { 
+                    visited[n] = true; 
+                    queue.add(n); 
+                } 
+            } 
+      
+        } 
+		
+		
+    }
 	void printGraph( ) {
 		
 		for (int i=0; i < list.length;i++) {
